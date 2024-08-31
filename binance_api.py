a6850f8cf3000b5c51fe461413b6801ea81472c1
Ulyana Sakhaschik <ulyana.sakhaschik@myoffice.team>
2024-08-31 22:39:38 +0300

    Add check error

--- binance_api.py
+++ binance_api.py
@@ -10,30 +10,47 @@ load_dotenv()
 num_connect = int(os.getenv('NUM_CONNECTIONS'))
 period = int(os.getenv('COLLECTION_PERIOD'))
 url = os.getenv('BINANCE_WS_URL')
-stream_name = os.getenv('STREAM_NAME')
+stream_name = os.getenv('STREAM_NAME').split("@")[0].lower() + "@" + os.getenv('STREAM_NAME').split("@")[1]
 output_file = os.getenv('OUTPUT_FILE')
 
 
 async def subscribe(ws, id):
     await ws.send(json.dumps({"method": "SUBSCRIBE", "params": [stream_name], "id": id}))
+    response = await ws.recv()
+    response_data = json.loads(response)
+    if "result" in response_data and response_data["result"] is None:
+        print(f"Подключение {id}: Подписка на {stream_name} успешна.")
+        return True
+    else:
+        print(f"Подключение {id}: Подписка не удалась. Ответ сервера: {response}")
+        return False
      
 async def collect_data(connection_id):
-    async with websockets.connect(url) as websocket:
-        await subscribe(websocket, connection_id)
-        start_time = time.time()
-        data = []
-        while time.time() - start_time < period:
-            message = await websocket.recv()
-            data.append(json.loads(message))
-        return data
+    try:
+        async with websockets.connect(url) as websocket:
+            print(f"Подключение {connection_id}: Подключено к WebSocket.")
+            if await subscribe(websocket, connection_id):
+                start_time = time.time()
+                data = []
+                while time.time() - start_time < period:
+                    message = await websocket.recv()
+                    data.append(json.loads(message))
+                return data
+            else:
+                return []
+    except Exception as e:
+        print(f"Подключение {connection_id}: Произошла ошибка: {e}")
+        return []
 
 async def main():
     tasks = [collect_data(i) for i in range(num_connect)]
-    all_data = await asyncio.gather(*tasks)
-
-    print(f"Собранные данные записаны в JSON файл ./{output_file}")
-    with open(output_file, "w") as f:
-        json.dump(all_data, f, indent=4)
+    if tasks:
+        all_data = await asyncio.gather(*tasks)
+        with open(output_file, "w") as f:
+            json.dump(all_data, f, indent=4)
+        print(f"Собранные данные записаны в JSON файл ./{output_file}")
+    else:
+        print(f"Не удалось получить данные из раздачи маркет даты")
 
 
 if __name__ == "__main__":
